3ff8a48e10d6ad1a0301dcfd37f44cc7a9974511
Luis Lantigua <luis_alexander2013@hotmail.com>
2019-09-17 12:36:55 -0400

    Inicando el primer proyecto de Unity

---
+++ TRPG/TRPG/Assets/Scripts/PiraPong/BallBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    float _startingForceScalar = 5f;
+    Vector3 startingForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1") && transform.parent != null)
+        {
+            startingForce = Vector3.zero;
+            startingForce.x = transform.parent.name == "Player1" ? -1 : 1;
+            startingForce.y = Random.Range(0, 2) == 0 ? 1 : -1;
+            startingForce *= _startingForceScalar;
+            transform.SetParent(null);
+            //GetComponent<Rigidbody>().AddForce(startingForce);
+            GetComponent<Rigidbody>().velocity = startingForce;
+        }
+    }
+}
---
+++ TRPG/TRPG/Assets/Scripts/PiraPong/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    bool _isPlayerOne;
+    float _movementSpeed = 1f;
+    const float _LOWERLIMIT = -3.455f, _UPPERLIMIT = 3.455f;
+    Vector3 _deltaPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _isPlayerOne = name == "Player1";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _deltaPos = new Vector3(0, _movementSpeed * Input.GetAxis(_isPlayerOne ? "Player1" : "Player2"));
+        transform.Translate(_deltaPos);
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, _LOWERLIMIT, _UPPERLIMIT), transform.position.z);
+    }
+}
